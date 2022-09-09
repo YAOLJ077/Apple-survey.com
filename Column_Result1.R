cd96843c8e47767706cec94358967df3e8eb284e
yaolinjia <18691945525@163.com>
2022-09-09 22:05:43 +0800

    格式更改

--- Column_Result1.R
+++
@@ -1,7 +0,0 @@
-mydata<-read.csv("F:/R/Data_bank/Column.csv")
-library(ggplot2)
-library(dplyr)
-library(ggsci)
-Col6<-pal_npg("nrc", alpha = 0.7)(5)
-ggplot(data=mydata,aes(Source,Count,group=Stage,fill=Stage))+
-  geom_bar(stat="identity",position="stack", colour="black", width=0.7,size=0.25)+scale_fill_manual(values = Col6)+labs(x="Information Source")+geom_text(aes(label=Count),position=position_stack(vjust=0.5))
